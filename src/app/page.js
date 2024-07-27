0db5191db877f13d43f949aacb27c027d016ef81
Maaz Shamim <m.shamim.22792@khi.iba.edu.pk>
2024-07-27 05:17:40 +0500

    Landing Page Complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
+import Form from "@/components/Form";
 
 export default function Home() {
   return (
-    <div className="bg-[#F9F9FF] min-h-screen overflow-y-auto">
+    <div className="bg-[#f1f2f3] min-h-screen overflow-hidden">
       <Header/>
       <Main/>
+      <Form/>
       <Footer/>
     </div>
   );
---
+++ src/components/Form.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+export const Form = () => 
+{
+    return (
+		<div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:text-gray-800">
+
+			<div className="flex flex-col justify-between">
+				<div className="space-y-2">
+					<h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's Begin!</h2>
+					<div className="dark:text-gray-600">Let us know about your project. We'd love to hear from you!</div>
+				</div>
+				<img src="doodle.svg" alt="" className="p-6 h-52 md:h-64" />
+			</div>
+			<form noValidate="" className="space-y-6">
+				<div>
+					<label htmlFor="name" className="text-sm">Full name</label>
+					<input id="name" type="text" placeholder="" className="w-full p-3 rounded bg-white" />
+				</div>
+				<div>
+					<label htmlFor="email" className="text-sm">Email</label>
+					<input id="email" type="email" className="w-full p-3 rounded bg-white" />
+				</div>
+				<div>
+					<label htmlFor="message" className="text-sm">Message</label>
+					<textarea id="message" rows="3" className="w-full p-3 rounded bg-white"></textarea>
+				</div>
+				<button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-black text-white">Send Message</button>
+			</form>
+		</div>
+    );
+};
+
+export default Form
